c9c17707babe4eade1e0b6a9e412473611e91e7b
McYeremia <117885019+McYeremia@users.noreply.github.com>
2023-01-03 12:16:07 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,30 @@
+while pil1(pil1<3):
+    print("****** Kredit Keaktifan Mahasiswa ******")
+    print("Student Activities Credit")
+    print("1. Menambahkan Kegiatan")
+    print("2. Menampilkan Jumlah Poin")
+    print("3. Keluar")
+    pil1= int(input("Silahkan masukan pilihan yang Anda: "))
+    if pil1 == 1:
+        str(input("Nama Kegiatan:"))
+        (input("Tanggal Kegiatan:"))
+        print("Pilihan Kategori Kegiatan :")
+        print(" -Prestasi")
+        print(" -Organisasi")
+        print(" -Kepanitiaan")
+        print(" -Rekognisi")
+        namakegiatan=str(input("Masukan Kategori Kegiatan:"))
+        print("Kegiatan berhasil ditambahkan")
+
+    elif pil1 == 2:
+        str(input("Nama Kegiatan:"))
+        (input("Tanggal Kegiatan:"))
+        print("Pilihan Kategori Kegiatan :")
+        print(" -Prestasi")
+        print(" -Organisasi")
+        print(" -Kepanitiaan")
+        print(" -Rekognisi")
+        namakegiatan=str(input("Masukan Kategori Kegiatan:"))
+
+    elif pil1 == 3:
+        print("Sistem Berhenti...")
